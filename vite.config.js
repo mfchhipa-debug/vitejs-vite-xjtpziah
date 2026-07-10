1ee982b9aab6edd984575ab9f71117476677e193
mfchhipa-debug <mfchhipa@gmail.com>
2026-07-10 18:07:38 +0530

    "Final fix: CSS declarations and tsconfig"

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+export default defineConfig({
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['icon-192.png', 'icon-512.png'],
+            manifest: false, // हमने खुद manifest.json बनाया है
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+                runtimeCaching: [
+                    {
+                        urlPattern: /^https:\/\/firebasestorage\.googleapis\.com\/.*/i,
+                        handler: 'CacheFirst',
+                        options: {
+                            cacheName: 'firebase-storage',
+                            expiration: {
+                                maxEntries: 200,
+                                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+                            },
+                        },
+                    },
+                ],
+            },
+        }),
+    ],
+});
